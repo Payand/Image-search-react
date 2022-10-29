d781effb8302c55d191d4b6389562bdc079e5d41
Ali shooshtari <shooshtariali05@gmail.com>
2022-10-29 19:09:42 +0330

    project finished

--- src/components/ImageListComponents/ImageList.js
+++ src/components/ImageListComponents/ImageList.js
@@ -1,12 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./ImageList.css";
 const ImageList = ({ images }) => {
-  console.log(images);
+  const [show, setShow] = useState("");
+
   const img = images.map(({ id, urls, alt_description }) => (
-    <img key={id} src={urls.regular} alt={alt_description} />
+    <img
+      key={id}
+      src={urls.regular}
+      alt={alt_description}
+      onMouseOver={(e) => setShow(e.target.src)}
+    />
   ));
 
-  return <div>{img}</div>;
+  return (
+    <div className="show">
+      <div className="image-list">{img}</div>
+      <div>
+        <img className="big-show" src={show} alt={show.substring(0, 10)} />
+      </div>
+    </div>
+  );
 };
 
 export default ImageList;
